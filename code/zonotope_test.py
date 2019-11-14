9091364d0bd095bcacdde1da8ceef196dbef46da
Simone <simone.barbaro2@studio.unibo.it>
2019-11-14 17:55:48 +0100

    corrected test error

--- code/zonotope_test.py
+++ code/zonotope_test.py
@@ -19,10 +19,10 @@ class ZonotopeTest(unittest.TestCase):
         self.assertEqual(z.upper(), torch.tensor([[2]]))
 
         z = z * 2
-        self.assertEqual(z.a0, torch.tensor([[1]]))
-        self.assertEqual(z.A, torch.tensor([[1]]))
+        self.assertEqual(z.a0, torch.tensor([[2]]))
+        self.assertEqual(z.A, torch.tensor([[2]]))
         self.assertEqual(z.lower(), torch.tensor([[0]]))
-        self.assertEqual(z.upper(), torch.tensor([[2]]))
+        self.assertEqual(z.upper(), torch.tensor([[4]]))
 
 
 if __name__ == '__main__':
