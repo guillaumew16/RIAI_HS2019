ead49ccadc5f34648a3dd3b843a243bf06d3ca4a
Simone <simone.barbaro2@studio.unibo.it>
2019-11-21 22:06:06 +0100

    updated zonotope implementation

--- code/zonotope.py
+++ code/zonotope.py
@@ -25,7 +25,7 @@ class Zonotope:
         return Zonotope(self.A * other, self.a0 * other)
 
     def __getitem__(self, item):
-        return Zonotope(self.A[:, item], self.a0[:, item])
+        return Zonotope(self.A[:, item:item + 1], self.a0[:, item:item + 1])
 
     def sum(self):
         return Zonotope(self.A.sum(1, keepdim=True), self.a0.sum(1, keepdim=True))
@@ -59,13 +59,14 @@ class Zonotope:
         lambdas[:, zero_map] = u[:, zero_map] / (u[:, zero_map] - l[:, zero_map])
         return lambdas
 
-    def relu(self, lambdas):
+    def relu(self, lambdas=None):
         l = self.lower()
         u = self.upper()
 
         lower_map = (u <= 0)[0]
         upper_map = (l >= 0)[0]
         intersection_map = ((l < 0) * (u > 0))[0]
+
         new_epslon_size = torch.nonzero(intersection_map).size(0)
         new_A_shape = (self.A.shape[0] + new_epslon_size, *self.A.shape[1:])
         A = torch.zeros(new_A_shape)
@@ -78,15 +79,25 @@ class Zonotope:
         A[:self.A.shape[0], upper_map] = self.A[:, upper_map]
 
         breaking_point = u[:, intersection_map] / (u[:, intersection_map] - l[:, intersection_map])
-        use_l_map = lambdas[:, intersection_map] >= breaking_point
+        if lambdas is None:
+            mu[:, intersection_map] = l[:, intersection_map] * breaking_point / 2
 
-        tmp = torch.zeros(mu[:, intersection_map].shape)
-        tmp[use_l_map] = - l[:, intersection_map][use_l_map] * lambdas[:, intersection_map][use_l_map]
-        tmp[~ use_l_map] = u[:, intersection_map][~ use_l_map] * (1 - lambdas[:, intersection_map][~ use_l_map])
+            a0[:, intersection_map] = a0[:, intersection_map] * breaking_point + mu[:, intersection_map]
+            A[:self.A.shape[0], intersection_map] = self.A[:, intersection_map] * breaking_point
+        else:
+            use_l_map = lambdas[:, intersection_map] >= breaking_point
 
-        mu[:, intersection_map] = tmp / 2
+            tmp = torch.zeros(mu[:, intersection_map].shape)
+            tmp[use_l_map] = - l[:, intersection_map][use_l_map] * lambdas[:, intersection_map][use_l_map]
+            tmp[~ use_l_map] = u[:, intersection_map][~ use_l_map] * (1 - lambdas[:, intersection_map][~ use_l_map])
 
-        a0[:, intersection_map] = a0[:, intersection_map] * lambdas[:, intersection_map] + mu[:, intersection_map]
-        A[:self.A.shape[0], intersection_map] = self.A[:, intersection_map] * lambdas[:, intersection_map]
+            mu[:, intersection_map] = tmp / 2
+
+            a0[:, intersection_map] = a0[:, intersection_map] * lambdas[:, intersection_map] + mu[:, intersection_map]
+            A[:self.A.shape[0], intersection_map] = self.A[:, intersection_map] * lambdas[:, intersection_map]
+
         A[self.A.shape[0]:, intersection_map] = torch.diag((mu[:, intersection_map]).reshape(-1))
         return Zonotope(A, a0)
+
+    def __str__(self):
+        return "Zonotope center: {}, epsilon coefficients: {}".format(self.a0, self.A)
