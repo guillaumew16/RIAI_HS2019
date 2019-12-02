6345b0a1e5fbad9d89d711d56bd6d3e8b6295e84
guiwang <guiwang@student.ethz.ch>
2019-12-02 20:31:59 +0100

    cache out_dim in zmodules.py

--- code/zmodules.py
+++ code/zmodules.py
@@ -14,11 +14,17 @@ class _zModule(nn.Module):
     def __init__(self):
         super().__init__()
         self.in_dim = None # can be interpreted as "any"...
+        self.__out_dim = None
 
     def __str__(self):
         return super().__str__() + " in_dim={} out_dim={}".format(self.in_dim, self.out_dim())
 
     def out_dim(self):
+        if self.__out_dim is None:
+            self.__out_dim = self._get_out_dim()
+        return self.__out_dim
+    
+    def _get_out_dim(self):
         raise NotImplementedError("Please implement this method")
 
 # The only _zModule that has parameters
@@ -36,7 +42,7 @@ class zReLU(_zModule):
         self.__uninitialized = True
         self.lambda_layer = nn.Parameter(torch.zeros(1, *in_dim), requires_grad=True)
     
-    def out_dim(self):
+    def _get_out_dim(self):
         return self.in_dim
 
     def forward(self, zonotope):
@@ -57,25 +63,15 @@ class zNormalization(_zModule):
         super().__init__()
         self.mean = mean
         self.sigma = sigma
-
-    # Python doesn't support __init__ overload
-    # def __init__(self, concrete_layer):
-    #     super().__init__()
-    #     if not isinstance(concrete_layer, Normalization):
-    #         raise ValueError("expected a networks.Normalization")
-    #     self.mean = concrete_layer.mean
-    #     self.mean = concrete_layer.sigma
-
-    def out_dim(self):
+    def _get_out_dim(self):
         return self.in_dim
-
     def forward(self, zonotope):
         return zonotope.normalization(self.mean, self.sigma)
 
 class zFlatten(_zModule):
     def __init__(self):
         super().__init__()
-    def out_dim(self):
+    def _get_out_dim(self):
         if self.in_dim is None:
             import warnings
             warnings.warn("Attribute self.in_dim should have been initialized, but is still =None. returning out_dim=None")
@@ -89,21 +85,11 @@ class zLinear(_zModule):
         super().__init__()
         self.weight = weight.detach()
         self.bias = bias.detach()
-
-    # Python doesn't support __init__ overload
-    # def __init__(self, concrete_layer):
-    #     super().__init__()
-    #     if not isinstance(concrete_layer, nn.Linear):
-    #         raise ValueError("expected a nn.Linear")
-    #     self.weight = concrete_layer.weight
-    #     self.bias = concrete_layer.bias
-
-    def out_dim(self):
-        # take advantage of this call to make a sanity-check on in_dim
+    def _get_out_dim(self):
+        # take advantage of this call to make a sanity-check on in_dim. for DEBUG only
         assert len(self.in_dim) == 1
         assert self.in_dim[0] == self.weight.shape[1]
         return self.weight.shape[:1]
-
     def forward(self, zonotope):
         return zonotope.linear_transformation(self.weight, self.bias)
 
@@ -137,7 +123,7 @@ class zConv2d(_zModule):
         
         self.__concrete_layer = concrete_layer # TODO: once possible, remove this. # TODO: detach concrete_layer's parameters...
 
-    def out_dim(self):
+    def _get_out_dim(self):
         if self.__out_dim is not None:
             return self.__out_dim
         # TODO: maybe try to find a better way to do this... On the other hand multiplying by 0 is probably optimized out by pyTorch
