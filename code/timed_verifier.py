77be058d1d39e6db9c69379e79a0f896a3520560
guiwang <guiwang@student.ethz.ch>
2019-12-16 21:37:06 +0100

    write script my_evaluate_preliminary_submission

---
+++ code/timed_verifier.py
@@ -0,0 +1,80 @@
+import argparse
+import torch
+from networks import FullyConnected, Conv
+from analyzer import Analyzer
+
+DEVICE = 'cpu'
+INPUT_SIZE = 28
+
+
+def analyze(net, inputs, eps, true_label, verbose=False, net_name=None):
+    ana = Analyzer(net, inputs, eps, true_label)
+    if verbose:
+        gv_path = "../graphviz_outputs/"
+        if net_name is not None:
+            gv_path += net_name + "/"
+        ana.make_dot_loss(gv_path + "loss.gv")
+        ana.make_dot_znet(gv_path + "znet.gv")
+        ana.make_dot_concrete(gv_path + "concrete_net.gv")
+    return ana.analyze(verbose=verbose)
+    # return Analyzer(net, inputs, eps, true_label).analyze(verbose=verbose)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Neural network verification using DeepZ relaxation')
+    parser.add_argument('--net',
+                        type=str,
+                        choices=['fc1', 'fc2', 'fc3', 'fc4', 'fc5', 'conv1', 'conv2', 'conv3', 'conv4', 'conv5'],
+                        required=True,
+                        help='Neural network to verify.')
+    parser.add_argument('--spec', type=str, required=True, help='Test case to verify.')
+    parser.add_argument('--myverbose', 
+                        action='store_true', # False by default
+                        help="Run the analyzer verbosely. (student-defined flag)")
+    args = parser.parse_args()
+
+    with open(args.spec, 'r') as f:
+        lines = [line[:-1] for line in f.readlines()]
+        true_label = int(lines[0])
+        pixel_values = [float(line) for line in lines[1:]]
+        eps = float(args.spec[:-4].split('/')[-1].split('_')[-1])
+
+    if args.net == 'fc1':
+        net = FullyConnected(DEVICE, INPUT_SIZE, [100, 10]).to(DEVICE)
+    elif args.net == 'fc2':
+        net = FullyConnected(DEVICE, INPUT_SIZE, [50, 50, 10]).to(DEVICE)
+    elif args.net == 'fc3':
+        net = FullyConnected(DEVICE, INPUT_SIZE, [100, 100, 10]).to(DEVICE)
+    elif args.net == 'fc4':
+        net = FullyConnected(DEVICE, INPUT_SIZE, [100, 100, 100, 10]).to(DEVICE)
+    elif args.net == 'fc5':
+        net = FullyConnected(DEVICE, INPUT_SIZE, [400, 200, 100, 100, 10]).to(DEVICE)
+    elif args.net == 'conv1':
+        net = Conv(DEVICE, INPUT_SIZE, [(32, 4, 2, 1)], [100, 10], 10).to(DEVICE)
+    elif args.net == 'conv2':
+        net = Conv(DEVICE, INPUT_SIZE, [(32, 4, 2, 1), (64, 4, 2, 1)], [100, 10], 10).to(DEVICE)
+    elif args.net == 'conv3':
+        net = Conv(DEVICE, INPUT_SIZE, [(32, 3, 1, 1), (32, 4, 2, 1), (64, 4, 2, 1)], [150, 10], 10).to(DEVICE)
+    elif args.net == 'conv4':
+        net = Conv(DEVICE, INPUT_SIZE, [(32, 4, 2, 1), (64, 4, 2, 1)], [100, 100, 10], 10).to(DEVICE)
+    elif args.net == 'conv5':
+        net = Conv(DEVICE, INPUT_SIZE, [(16, 3, 1, 1), (32, 4, 2, 1), (64, 4, 2, 1)], [100, 100, 10], 10).to(DEVICE)
+
+    net.load_state_dict(torch.load('../mnist_nets/%s.pt' % args.net, map_location=torch.device(DEVICE)))
+
+    inputs = torch.FloatTensor(pixel_values).view(1, 1, INPUT_SIZE, INPUT_SIZE).to(DEVICE)
+    outs = net(inputs)
+    pred_label = outs.max(dim=1)[1].item()
+    assert pred_label == true_label
+
+    if analyze(net, inputs, eps, true_label, args.myverbose, args.net):
+        print('verified')
+    else:
+        print('not verified')
+
+
+if __name__ == '__main__':
+    import time
+    start_time = time.time()
+    main()
+    print("--- verifier execution time: %s seconds ---" % (time.time() - start_time))
